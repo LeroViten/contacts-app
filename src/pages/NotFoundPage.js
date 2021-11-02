64c75c717ba170a901f16aed821c149dc7523592
Valerii Vitenko <valero2313@gmail.com>
2021-11-02 20:15:18 +0200

    fixed redirect to main while on NotFoundPage

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -5,7 +5,7 @@ export default function NotFoundPage() {
     <>
       <h1>The page you're looking for is not found! 😢</h1>
       <p>
-        Go back to <Link to="/" /> main page
+        Go back to <Link to="/"> main page </Link>
       </p>
     </>
   );
